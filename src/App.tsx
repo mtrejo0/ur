d55ec48f0cace9c24e30d19c63f15461b023547c
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2023-05-10 19:03:55 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const [dice, setDice] = useState([0,0,0,0])
+
+  const [rolling, setRolling] = useState(false)
+
+  const roll = () => {
+
+
+    setRolling(true)
+
+    setTimeout(() => {
+
+      setRolling(false)
+
+    }, 300)
+
+
+
+    const newState = [0,0,0,0]
+
+    for (let i = 0; i < 4; i++) {
+      
+
+      const s = Math.random() < .5 ? 1 : 0
+
+      newState[i] = s
+      
+    }
+
+    setDice(newState)
+
+
+  }
+
+  const total = () => dice.reduce((p, a) => a+p)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href={"https://playur.org/"}><h2>Play UR</h2></a>
+
+
+      <h3>Dice</h3>
+
+      {dice.map((die, i) => {
+        return <h3>{die}</h3>
+      })}
+
+
+      <h2>Total: {total()}</h2>
+      
+
+
+      <button onClick={() => {
+        roll()
+      }}>Roll</button>
+
+      {rolling && <p>rolling...</p>}
     </div>
   );
 }
